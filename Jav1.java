90244cc0fbf1b20bca55f398bb2c0c6a52ff1f0e
Adarsh7898 <143689447+Adarsh7898@users.noreply.github.com>
2023-08-31 12:41:02 +0530

    Add files via upload

---
+++ Jav1.java
@@ -0,0 +1,18 @@
+
+
+
+
+import java.util.Scanner;
+
+
+public class Jav1
+
+{ public static void main(String[]args){
+    System.out.println("Enter any integer to display");
+    Scanner s= new Scanner(System.in);
+    int a= s.nextInt();
+    System.out.println("The integer is "+a);}
+    
+    
+}
+
