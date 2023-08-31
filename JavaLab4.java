219582d3a090b249a3cb4d7d8d9548e57f2d3caa
Adarsh7898 <143689447+Adarsh7898@users.noreply.github.com>
2023-08-31 12:54:34 +0530

    Checking whether a number is odd or even

---
+++ JavaLab4.java
@@ -0,0 +1,27 @@
+
+
+
+
+import java.util.Scanner;
+
+
+public class JavaLab4
+
+{ public static void main(String[]args){
+    int c;
+    System.out.println("Enter a number to check");
+    Scanner s= new Scanner(System.in);
+    int a= s.nextInt();
+    if(a%2==0)
+    {
+         System.out.println("The number is even");
+    }
+    
+else
+   
+    { System.out.println("The number is odd" );
+}
+    
+    
+}}
+
